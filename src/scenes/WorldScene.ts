22c231fa4802e0421f2eba818e72440cf4d50050
Sini Nysten <sini.nysten@taitounited.fi>
2020-11-17 19:30:44 +0200

    improve cow art

--- src/scenes/WorldScene.ts
+++ src/scenes/WorldScene.ts
@@ -37,8 +37,20 @@ export default class WorldScene extends Phaser.Scene {
     npc1.generateTexture('npc1', 40, 40);
 
     const cow = new Phaser.GameObjects.Graphics(this);
-    cow.fillStyle(0xffff00);
+    cow.fillStyle(0xffffff);
     cow.fillCircle(20, 20, 20);
+    cow.fillStyle(0x333333);
+    for (let i = 0; i < 15; i++) {
+      const size = Math.max(Math.random() * 8, 4);
+      const dist = Math.random() * (20 - size);
+      const ang = Math.random() * Math.PI * 2;
+
+      cow.fillCircle(
+        20 + Math.cos(ang) * dist,
+        20 + Math.sin(ang) * dist,
+        size
+      );
+    }
     cow.generateTexture('cow', 40, 40);
 
     const bullet = new Phaser.GameObjects.Graphics(this);
