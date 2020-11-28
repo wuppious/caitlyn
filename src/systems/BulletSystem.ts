097b4ccbffb5e00c6b43531f7551db1e015e9ed5
Sini Nysten <sini.nysten@gmail.com>
2020-11-28 22:06:12 +0200

    remove bullet move on collision

--- src/systems/BulletSystem.ts
+++ src/systems/BulletSystem.ts
@@ -26,6 +26,8 @@ export default class BulletSystem extends System {
           target.addComponent<Damage>(Damage, { points: bullet.damage });
         }
       });
+
+      entity.removeComponent(Move);
     });
   }
 }
